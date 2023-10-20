4e5e3c7a9c63d966c02f3bd1a4aa6e59d07d78a7
Exequiel <alvarado.exequiel@gmail.com>
2023-10-20 20:20:35 -0300

    servicios y validacion
    recuperacion

--- RegisterApp/src/app/app-routing.module.ts
+++ RegisterApp/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'error',
+    loadChildren: () => import('./error/error.module').then( m => m.ErrorPageModule)
+  },
 ];
 
 @NgModule({
